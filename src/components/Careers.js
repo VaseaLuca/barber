5a2e428106a1c074c77d78d0bd2572a08de62191
Basil <Vaseokk@gmail.com>
2020-12-09 18:00:33 +0200

    update

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import Footer from './Home/Footer';
+import career_photo from "./assets/career_photo.jpg";
+import './Careers.scss';
+
 function Careers() {
   return (
-    <div>
-      Careers
+    <div className='career'>
+      <div className="career-header">
+        Careers
+      </div>
+      <div className="career-block">
+        <div className='career-description'>
+          Momentan nu este deschisă nici o poziție.
+          Urmărește-ne pe instagram sau facebook pentru update.
+        </div>
+        <div className='career-img'><img src={career_photo} alt=""/></div>
+      </div>
+      <Footer />
     </div>
   )
 }
