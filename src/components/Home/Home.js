da94743c13f129581680d1bd75b89ae6081bf1ec
Basil <Vaseokk@gmail.com>
2020-12-10 13:01:16 +0200

    added lazy loading on home components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
-import Greetings from './Greetings';
-import About from './About';
-import Career from './About';
-import ServicePrice from './Service_Price';
-import Footer from './Footer';
+// import Greetings from './Greetings';
+// import About from './About';
+// import Career from './About';
+// import ServicePrice from './Service_Price';
+// import Footer from './Footer';
 import about_img from "../assets/Octavian&Madalina.jpg";
 import career_img from "../assets/career_photo.jpg";
 import './Home.scss';
+const Greetings = React.lazy(() => import("./Greetings"));
+const About = React.lazy(() => import('./About'));
+const Career = React.lazy(() => import('./About'));
+const ServicePrice = React.lazy(() => import('./Service_Price'));
+const Footer = React.lazy(() => import('./Footer'));
 
 function Home() {
   return (
     <div className="home">
-      <Greetings />
+      <Suspense fallback={ <div>Loading...</div> }>
+        <Greetings />
       <About
         title={"Despre Noi"}
         info={"Armonia perfectă între BarberShop & Coafor"}
@@ -27,6 +33,7 @@ function Home() {
         directory={"/cariere"}
       />
       <Footer />
+      </Suspense>
     </div>
   );
 }
