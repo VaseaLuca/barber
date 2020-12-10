66e6ea9dc3796ddfbe470eb6f025225d9d22e3b6
Basil <Vaseokk@gmail.com>
2020-12-10 16:33:21 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
-import React,{ useState } from 'react';
+import React,{ useState, Suspense, lazy } from 'react';
 import { Route } from 'react-router-dom';
 
-import Backdrop from './Backdrop';
-import Navbar from './Navbar';
-import Drawer from './Drawer';
-import Home from './Home/Home';
-import IndicatiiMaps from './IndicatiiMaps';
-import About from './About';
-import PriceList from './PriceList';
-import Careers from './Careers';
-import ConfidentialPolicy from './ConfidentialPolicy';
-import TermsConditions from './TermsConditions';
+
+// import Navbar from './Navbar';
+// import Backdrop from "./Backdrop";
+// import Drawer from "./Drawer";
+// import Home from './Home/Home';
+// import IndicatiiMaps from './IndicatiiMaps';
+// import About from './About';
+// import PriceList from './PriceList';
+// import Careers from './Careers';
+// import ConfidentialPolicy from './ConfidentialPolicy';
+// import TermsConditions from './TermsConditions';
 import './App.scss';
+const Backdrop = lazy(() => import("./Backdrop"));
+const Drawer = lazy(() => import("./Drawer"));
+const Navbar = lazy(()=> import('./Navbar'));
+const Home = lazy(() => import("./Home/Home"));
+const IndicatiiMaps = lazy(() => import("./IndicatiiMaps"));
+const About = lazy(() => import("./About"));
+const PriceList = lazy(() => import("./PriceList"));
+const Careers = lazy(() => import("./Careers"));
+const ConfidentialPolicy = lazy(() => import("./ConfidentialPolicy"));
+const TermsConditions = lazy(() => import("./TermsConditions"));
+
 
 function App() {
 
@@ -20,6 +32,7 @@ const [drawerOpen, setDrawerOpen]= useState(false);
 
   return (
     <div className="App">
+ <Suspense fallback={<div>Loading...</div>}>
       <Backdrop open={drawerOpen} close={()=> setDrawerOpen(!drawerOpen)} />
       <Navbar isDrawerOpen={drawerOpen} openDrawer={()=> setDrawerOpen(!drawerOpen)} />
       <Drawer drawerIsOpen={drawerOpen} close={()=> setDrawerOpen(!drawerOpen)} />
@@ -30,6 +43,7 @@ const [drawerOpen, setDrawerOpen]= useState(false);
       <Route path='/cariere' exact component={Careers} />
       <Route path='/politica-de-confidentialitate' exact component={ConfidentialPolicy} />
       <Route path='/termeni-si-conditii' exact component={TermsConditions} />
+    </Suspense>
 
 
     </div>
